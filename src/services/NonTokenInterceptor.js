508f7c5f1ba2fb0c9d331d0d000fb3a4dcada36f
attowls <planting31@ajou.ac.kr>
2023-05-16 16:07:51 +0900

    Chore: axios 및 axios interceptor 적용

---
+++ src/services/NonTokenInterceptor.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const NonTokenInterceptor = axios.create({
+  baseURL: process.env.REACT_APP_BASEURL,
+  timeout: 15000,
+});
+
+// request interceptors
+NonTokenInterceptor.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+NonTokenInterceptor.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    const status = error.response.status;
+
+    switch (status) {
+
+      case 401: {
+        window.location.href = '/';
+        return Promise.reject(error.message);
+      }
+
+      case 403: {
+        window.location.href = "/";
+        return Promise.reject(error.message);
+      }
+
+      default : {
+        return Promise.reject(error.message);
+      }
+    }
+  }
+);
+
+export default NonTokenInterceptor;
