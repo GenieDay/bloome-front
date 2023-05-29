4fa307e4e798c605e866196ea68b1a7c47c9e7ac
attowls <planting31@ajou.ac.kr>
2023-05-29 21:40:13 +0900

    Update: 테스트 화면 닉네임 글자수 제한

--- src/components/Test/TesterDataForm.jsx
+++ src/components/Test/TesterDataForm.jsx
@@ -15,8 +15,11 @@ export default function TesterDataForm(props) {
         ? toString(nickname).replace(/^\s+|\s+$/gm, "")
         : nickname.replace(/^\s+|\s+$/gm, "");
 
+
     if (valueWithNoSpace === "") {
       setNicknameValid(-2);
+    } else if (nickname.length > 20){
+      setNicknameValid(-3);
     } else {
       setNicknameValid(1);
       props.setTesterNickname(nickname);
@@ -24,20 +27,23 @@ export default function TesterDataForm(props) {
     }
   };
 
+
   return (
     <Page.RoundedBox style={{ height: "250px" }}>
       {/* <Form.TestFormBound> */}
       <Form.TestFormTitle>닉네임</Form.TestFormTitle>
       <Form.TestFormSubTitle>
-        꽃과 함께 남길 닉네임을 입력해주세요.
+        꽃과 함께 남길 '20자 이하의' 닉네임을 입력해주세요.
       </Form.TestFormSubTitle>
       <Form.InputBox
         type="text"
         onChange={(e) => setNickname(e.target.value)}
-        style={{width: "80%"}}
+        style={{ width: "80%" }}
       />
       {nicknameValid === -2 ? (
         <Form.SubTextRed>닉네임을 입력해주세요.</Form.SubTextRed>
+      ) : nicknameValid === -3 ? (
+        <Form.SubTextRed>20자 이하로 입력해주세요.</Form.SubTextRed>
       ) : (
         <Form.SubTextRed style={{ visibility: "hidden" }}>
           아이디를 입력해주세요.
--- src/pages/Garden/GardenPage.jsx
+++ src/pages/Garden/GardenPage.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+// css
+import * as Page from "../../styles/Page";
+import * as Form from "../../styles/Form";
+import "../../styles/Common.css";
+
 export default function GardenPage() {
-  const params = useParams().userId;
+  const userNickname = useParams().userId;
 
   return (
     <React.Fragment>
-      {params}
+      <Page.Background>
+        {/* 타이틀 */}
+        <div
+          style={{
+            position: "absolute",
+            top: "10%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div className={"content-title"}>{userNickname}님의 정원</div>
+          <div className={"content-subtitle"}>n개의 꽃이 피었습니다!</div>
+        </div>
+        {/* 꽃 심기 버튼 - 방문자만 */}
+        <Form.SubmitButton
+          style={{ position: "absolute", bottom: "10%", width: "200px" }}
+        >
+          꽃 심기
+        </Form.SubmitButton>
+      </Page.Background>
     </React.Fragment>
-  )
+  );
 }
